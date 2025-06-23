4d60be87be997faf5787e00ad05be53b4704973d
Atharv Karbhari <59548859+AKAMasterMind404@users.noreply.github.com>
2025-06-23 22:30:53 +0530

    Migration

--- lib/data.dart
+++ lib/data.dart
@@ -1,77 +1,25 @@
-String amountParser(int amount){
-  if(amount<1000){return amount.toString();}
+String amountParser(int amount) {
+  if (amount < 1000) {
+    return amount.toString();
+  }
 
   String newamount = amount.toString();
 
-  int rem = newamount.length%3;
+  int rem = newamount.length % 3;
 
-  String init = newamount.substring(0,rem);
+  String init = newamount.substring(0, rem);
   String remain = newamount.substring(rem);
 
-  init!=""?init+=",":null;
+  init != "" ? init += "," : null;
 
   String ans = "";
 
-  for(int i=0;i<remain.length;i++){
-    ans+=remain[i];
-    if(i+1!=remain.length && (i+1)%3==0){
-      ans+=",";
+  for (int i = 0; i < remain.length; i++) {
+    ans += remain[i];
+    if (i + 1 != remain.length && (i + 1) % 3 == 0) {
+      ans += ",";
     }
   }
 
   return init + ans;
 }
-
-class CardData {
-  int price;
-  String imageURL, name;
-  int bought = 0;
-
-  CardData({this.price, this.imageURL, this.name, this.bought = 0});
-}
-
-CardData c1 = CardData(price: 2, name: "Big Mac", imageURL: "lib/assets/Images/bigmac.png");
-CardData c2 = CardData(price: 3, name: "Flip Flops", imageURL: "lib/assets/Images/flipflops.jpg");
-CardData c3 = CardData(price: 5, name: "Coca-Cola Pack", imageURL: "lib/assets/Images/cocacolapack.jpeg");
-CardData c4 = CardData(price: 12, name: "Movie Ticket", imageURL: "lib/assets/Images/movieticket.jpg");
-CardData c5 = CardData(price: 15, name: "Book", imageURL: "lib/assets/Images/book.jpg");
-CardData c6 = CardData(price: 45, name: "Lobster Dinner", imageURL: "lib/assets/Images/lobsterdinner.jpg");
-CardData c7 = CardData(price: 60, name: "Video Game", imageURL: "lib/assets/Images/videogame.jpg");
-CardData c8 = CardData(price: 99, name: "Amazon Echo", imageURL: "lib/assets/Images/amazonecho.jpg");
-CardData c9 = CardData(price: 100, name: "Year of Netflix", imageURL: "lib/assets/Images/netflix.png");
-CardData c10 = CardData(price: 125, name: "Air Jordans", imageURL: "lib/assets/Images/airjordans.jpg");
-CardData c11 = CardData(price: 199, name: "Air Pods", imageURL: "lib/assets/Images/airpods.jpg");
-CardData c12 = CardData(price: 299, name: "Gaming Console", imageURL: "lib/assets/Images/gamingconsole.jpg");
-CardData c13 = CardData(price: 350, name: "Drone", imageURL: "lib/assets/Images/drone.jpg");
-CardData c14 = CardData(price: 699, name: "Smartphone", imageURL: "lib/assets/Images/smartphone.jpg");
-CardData c15 = CardData(price: 800, name: "Bike", imageURL: "lib/assets/Images/bicycle.jpg");
-CardData c16 = CardData(price: 1500, name: "Kitten", imageURL: "lib/assets/Images/kitten.jpg");
-CardData c17 = CardData(price: 1500, name: "Puppy", imageURL: "lib/assets/Images/puppy.jpg");
-CardData c18 = CardData(price: 2300, name: "Auto Rickshaw", imageURL: "lib/assets/Images/autorickshaw.jpg");
-CardData c19 = CardData(price: 2500, name: "Horse", imageURL: "lib/assets/Images/horse.jpg");
-CardData c20 = CardData(price: 3000, name: "Acre of Farmland", imageURL: "lib/assets/Images/farmland.jpeg");
-CardData c21 = CardData(price: 5500, name: "Designer Handbag", imageURL: "lib/assets/Images/handbag.jpg");
-CardData c22 = CardData(price: 6000, name: "Hot Tub", imageURL: "lib/assets/Images/hottub.jpg");
-CardData c23 = CardData(price: 7000, name: "Luxury Wine", imageURL: "lib/assets/Images/wine.jpg");
-CardData c24 = CardData(price: 10000, name: "Diamond Ring", imageURL: "lib/assets/Images/diamondring.jpg");
-CardData c25 = CardData(price: 12000, name: "Jetski", imageURL: "lib/assets/Images/jetski.jpg");
-CardData c26 = CardData(price: 15000, name: "Rolex", imageURL: "lib/assets/Images/rolex.jpg");
-CardData c27 = CardData(price: 30000, name: "Ford F-150", imageURL: "lib/assets/Images/ford.jpg");
-CardData c28 = CardData(price: 75000, name: "Tesla", imageURL: "lib/assets/Images/tesla.jpg");
-CardData c29 = CardData(price: 150000, name: "Monster Truck", imageURL: "lib/assets/Images/monstertruck.jpg");
-CardData c30 = CardData(price: 250000, name: "Ferrari", imageURL: "lib/assets/Images/ferrari.jpg");
-CardData c31 = CardData(price: 3000000, name: "Single Family Home", imageURL: "lib/assets/Images/home.jpg");
-CardData c32 = CardData(price: 7000000, name: "Gold Bar", imageURL: "lib/assets/Images/goldbar.jpg");
-CardData c33 = CardData(price: 1500000, name: "McDonalds Franchise", imageURL: "lib/assets/Images/mcdonalds.jpg");
-CardData c34 = CardData(price: 5250000, name: "Superbowl Ad", imageURL: "lib/assets/Images/superbowlAD.jpg");
-CardData c35 = CardData(price: 7500000, name: "Yacht", imageURL: "lib/assets/Images/yatch.jpg");
-CardData c36 = CardData(price: 8000000, name: "M1 Abrams", imageURL: "lib/assets/Images/m1abrams.png");
-CardData c37 = CardData(price: 15000000, name: "Formula 1 Car", imageURL: "lib/assets/Images/f1car.jpg");
-CardData c38 = CardData(price: 31000000, name: "Apache Helicopter", imageURL: "lib/assets/Images/apacheHelicopter.jpg");
-CardData c39 = CardData(price: 45000000, name: "Mansion", imageURL: "lib/assets/Images/mansion.jpg");
-CardData c40 = CardData(price: 100000000, name: "Make a Movie", imageURL: "lib/assets/Images/movie.jpg");
-CardData c41 = CardData(price: 148000000, name: "Boeing 747", imageURL: "lib/assets/Images/boeing747.jpg");
-CardData c42 = CardData(price: 780000000, name: "Mona Lisa", imageURL: "lib/assets/Images/monalisa.jpg");
-CardData c43 = CardData(price: 850000000, name: "Sky Scraper", imageURL: "lib/assets/Images/skyscraper.jpg");
-CardData c44 = CardData(price: 930000000, name: "Cruise Ship", imageURL: "lib/assets/Images/cruise.jpg");
-CardData c45 = CardData(price: 2120000000, name: "NBA Team", imageURL: "lib/assets/Images/nbateam.jpg");
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:billgates/data.dart';
 
+import 'models/cardData.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -21,18 +23,7 @@ class mainPage extends StatefulWidget {
 class _mainPageState extends State<mainPage> {
   int amount = 100000000000;
 
-  List Datalist = <CardData>[
-    c1, c2, c3, c4, c5, c6, c7,
-    c8, c9, c10, c11, c12, c13,
-    c14, c15, c16, c17, c18, c19,
-    c20, c21, c22, c23, c24, c25,
-    c26, c27, c28, c29, c30, c31,
-    c32, c33, c34, c35, c36, c37,
-    c38, c39, c40, c41, c42, c43,
-    c44, c45
-  ];
-
-  @override
+    @override
   Widget build(BuildContext context) {
     double theWidth = MediaQuery.of(context).size.width;
     int axisCount = theWidth > 700 ? 3 : 2;
@@ -79,159 +70,157 @@ class _mainPageState extends State<mainPage> {
                 gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
                     crossAxisCount: axisCount),
                 itemCount: Datalist.length,
-                itemBuilder: (BuildContext context, int index) => Container(
-                      decoration: BoxDecoration(
-                          color: Colors.grey[200],
-                          borderRadius: BorderRadius.circular(10)),
-                      margin: EdgeInsets.all(10),
-                      padding: EdgeInsets.only(
-                          top: 10, bottom: 0, left: 10, right: 10),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          SizedBox(height: 5),
-                          Container(
-                            height:
-                                (MediaQuery.of(context).size.width / divideBy),
-                            width:
-                                (MediaQuery.of(context).size.width / divideBy),
-                            child: Container(
-                              decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(4),
-                                  image: DecorationImage(
-                                      fit: BoxFit.fill,
-                                      image: AssetImage(
-                                          Datalist[index].imageURL))),
-                            ),
-                          ), // IMAGE
+                itemBuilder: (BuildContext context, int index) {
+                  CardData currCard = Datalist[index];
+
+                  return Container(
+                    decoration: BoxDecoration(
+                        color: Colors.grey[200],
+                        borderRadius: BorderRadius.circular(10)),
+                    margin: EdgeInsets.all(10),
+                    padding: EdgeInsets.only(
+                        top: 10, bottom: 0, left: 10, right: 10),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        SizedBox(height: 5),
+                        Container(
+                          height:
+                              (MediaQuery.of(context).size.width / divideBy),
+                          width: (MediaQuery.of(context).size.width / divideBy),
+                          child: Container(
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(4),
+                                image: DecorationImage(
+                                    fit: BoxFit.fill,
+                                    image: AssetImage(currCard.imageURL))),
+                          ),
+                        ), // IMAGE
 //                        SizedBox(height: 5,),
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            children: [
-                              Text(
-                                "${Datalist[index].name}",
-                                style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: MediaQuery.of(context).size.width /
-                                      fontDivide,
-                                ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Text(
+                              "${currCard.name}",
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: MediaQuery.of(context).size.width /
+                                    fontDivide,
                               ),
-                              Text(
-                                "\$${amountParser(Datalist[index].price)}",
-                                style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: MediaQuery.of(context).size.width /
-                                      fontDivide,
-                                ),
+                            ),
+                            Text(
+                              "\$${amountParser(currCard.price)}",
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: MediaQuery.of(context).size.width /
+                                    fontDivide,
                               ),
-                            ],
-                          ),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceAround,
-                            children: [
-                              InkWell(
-                                onTap: () {
-                                  setState(() {
-
-                                    if( Datalist[index].bought > 0 ){
-                                      amount += Datalist[index].price;
-                                      Datalist[index].bought -= 1;
-                                    }
-
-                                  });
-                                },
-                                child: Container(
-                                  decoration: BoxDecoration(
-                                      color: Datalist[index].bought>0?
-                                              Colors.redAccent:
-                                              Colors.grey[700],
-                                      borderRadius: BorderRadius.circular(4)),
-                                  padding: EdgeInsets.only(
-                                      top: 4, bottom: 4, left: 2, right: 2),
-                                  height: MediaQuery.of(context).size.width /
-                                      buttonDivide,
-                                  width: MediaQuery.of(context).size.width /
-                                      (buttonDivide / 2),
-                                  child: Center(
-                                    child: Text(
-                                      "SELL",
-                                      style: TextStyle(
-                                          color: Colors.white,
-                                          fontSize: MediaQuery.of(context)
-                                                      .size
-                                                      .width /
-                                                  fontDivide -
-                                              4),
-                                    ),
-                                  ),
-                                ),
-                              ),
-                              Container(
+                            ),
+                          ],
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            InkWell(
+                              onTap: () {
+                                setState(() {
+                                  if (currCard.bought > 0) {
+                                    amount += currCard.price;
+                                    currCard.bought -= 1;
+                                  }
+                                });
+                              },
+                              child: Container(
                                 decoration: BoxDecoration(
-                                    color: Colors.white,
-                                    borderRadius: BorderRadius.circular(2)),
-                                padding: EdgeInsets.all(4),
-                                height: MediaQuery.of(context).size.width / 25,
-                                width: MediaQuery.of(context).size.width / 12.5,
+                                    color: currCard.bought > 0
+                                        ? Colors.redAccent
+                                        : Colors.grey[700],
+                                    borderRadius: BorderRadius.circular(4)),
+                                padding: EdgeInsets.only(
+                                    top: 4, bottom: 4, left: 2, right: 2),
+                                height: MediaQuery.of(context).size.width /
+                                    buttonDivide,
+                                width: MediaQuery.of(context).size.width /
+                                    (buttonDivide / 2),
                                 child: Center(
-                                  child: FittedBox(
-                                      fit: BoxFit.fitWidth,
-                                      child: Text("${Datalist[index].bought}",
-                                          style: TextStyle(
-                                              fontWeight: FontWeight.bold,
-                                              color: Colors.black,
-                                              fontSize: MediaQuery.of(context)
-                                                          .size
-                                                          .width /
-                                                      fontDivide -
-                                                  4))),
-                                ),
-                              ),
-                              InkWell(
-                                onTap: () {
-                                  setState(() {
-                                    int p = Datalist[index].price;
-                                    if(amount - p >= 0 ){
-                                      Datalist[index].bought += 1;
-                                      amount -= p;
-                                    }
-                                  });
-                                },
-                                child: Container(
-                                  decoration: BoxDecoration(
-                                      color: amount - Datalist[index].price >= 0?
-                                             Colors.greenAccent:
-                                             Colors.grey[700],
-                                      borderRadius: BorderRadius.circular(4)),
-                                  padding: EdgeInsets.only(
-                                      top: 4, bottom: 4, left: 2, right: 2),
-                                  height: MediaQuery.of(context).size.width /
-                                      buttonDivide,
-                                  width: MediaQuery.of(context).size.width /
-                                      (buttonDivide / 2),
-                                  child: Center(
-                                    child: Text(
-                                      "BUY",
-                                      style: TextStyle(
-                                          color: Colors.white,
-                                          fontSize: MediaQuery.of(context)
-                                                      .size
-                                                      .width /
-                                                  fontDivide -
-                                              4),
-                                    ),
+                                  child: Text(
+                                    "SELL",
+                                    style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize:
+                                            MediaQuery.of(context).size.width /
+                                                    fontDivide -
+                                                4),
                                   ),
                                 ),
                               ),
-                            ],
-                          ),
-                          SizedBox(
-                            height: 5,
-                          )
-                        ],
-                      ),
-                    ))
+                            ),
+                            Container(
+                              decoration: BoxDecoration(
+                                  color: Colors.white,
+                                  borderRadius: BorderRadius.circular(2)),
+                              padding: EdgeInsets.all(4),
+                              height: MediaQuery.of(context).size.width / 25,
+                              width: MediaQuery.of(context).size.width / 12.5,
+                              child: Center(
+                                child: FittedBox(
+                                    fit: BoxFit.fitWidth,
+                                    child: Text("${currCard.bought}",
+                                        style: TextStyle(
+                                            fontWeight: FontWeight.bold,
+                                            color: Colors.black,
+                                            fontSize: MediaQuery.of(context)
+                                                        .size
+                                                        .width /
+                                                    fontDivide -
+                                                4))),
+                              ),
+                            ),
+                            InkWell(
+                              onTap: () {
+                                setState(() {
+                                  int p = currCard.price;
+                                  if (amount - p >= 0) {
+                                    currCard.bought += 1;
+                                    amount -= p;
+                                  }
+                                });
+                              },
+                              child: Container(
+                                decoration: BoxDecoration(
+                                    color: amount - currCard.price >= 0
+                                        ? Colors.greenAccent
+                                        : Colors.grey[700],
+                                    borderRadius: BorderRadius.circular(4)),
+                                padding: EdgeInsets.only(
+                                    top: 4, bottom: 4, left: 2, right: 2),
+                                height: MediaQuery.of(context).size.width /
+                                    buttonDivide,
+                                width: MediaQuery.of(context).size.width /
+                                    (buttonDivide / 2),
+                                child: Center(
+                                  child: Text(
+                                    "BUY",
+                                    style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize:
+                                            MediaQuery.of(context).size.width /
+                                                    fontDivide -
+                                                4),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                        SizedBox(
+                          height: 5,
+                        )
+                      ],
+                    ),
+                  );
+                })
           ]),
         ),
       )),
